7e13b10fc316b38ba601183ec08101f933c1b529
kdvornichenko <gglade999@gmail.com>
2023-01-27 19:39:49 +0300

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -43,6 +43,7 @@ burger.addEventListener('click', () => {
 
 	currentLocationContainer.classList.toggle('visible')
 	document.body.classList.toggle('overflow-hidden')
+	document.documentElement.outerHTML.classList.toggle('overflow-hidden')
 
 	mobMenuVisibility()
 
